ecf6e9e7194ff3ba551256625f92060dd78e45c1
Thony Ivan Briza <brizathonyivand@gmail.com>
2024-11-21 05:27:53 +0800

    Update

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,10 @@
+// src/app/profile/layout.tsx
+
+export default function ProfileLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return <>{children}</>; // Render only children without any global layout
+  }
+    
