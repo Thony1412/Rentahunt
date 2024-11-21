8cfaa4a1180101d673c302ed718bee50d9eaf94c
Thony Ivan Briza <brizathonyivand@gmail.com>
2024-11-21 18:31:24 +0800

    Update

--- src/components/navprofile.tsx
+++ src/components/navprofile.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link';
+import { CgProfile } from "react-icons/cg";
+import { IoRocketOutline } from "react-icons/io5";
+import { IoShareSocialOutline } from "react-icons/io5";
 
 export default function Navprofile() {
     return(
-        <nav className="flex justify-between items-center p-4 bg-blue-500 text-white px-36">
+        <nav className="flex justify-between items-center p-4 bg-blue-500 text-white px-16">
             <div>
-            <Link href="/about" className="hover:underline mr-4">Services</Link>
-          <Link href="/rentees" className="hover:underline mr-4">About</Link>
-          <Link href="/rentees" className="hover:underline mr-4">Become a Rentereer</Link>
+            <Link href="/about" className="hover:underline mr-4">
+            <CgProfile className="inline-block mr-2" />
+            Profile
+            </Link>
+            
+            <Link href="/rentees" className="hover:underline mr-4">
+            <IoRocketOutline className="inline-block mr-2"/>
+            Services
+            </Link>
+
+            <Link href="/rentees" className="hover:underline mr-4">
+            <IoShareSocialOutline className="inline-block mr-2" />
+            Social
+            </Link>
             </div>
         </nav>
     );
